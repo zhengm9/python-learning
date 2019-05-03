9313f4c962d154f40e400b48556af51aa383661c
zmmason <390241476@qq.com>
2019-05-03 11:05:16 +0800

    1.first py commit file

---
+++ helloworld.py
@@ -0,0 +1,14 @@
+﻿import keyword
+import sys
+#keyword.kwlist
+print ("Hello, Python!") ;
+if True:
+	print("true");
+else:
+	print("false");
+str="hello world";
+print("test result:"+str[0:-1]);
+print("命令行参数为：");
+for i in sys.argv:
+	print (i);
+print("\npython·路径为：",sys.path);
